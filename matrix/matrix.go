4d142c39892909ece33031b6a818d58250ba6df1
hexagram <xml.hexagram@gmail.com>
2020-12-09 16:20:52 +0800

    add: matrix multiply

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,11 +1,19 @@
 package matrix
 
 import (
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrInvalidMatrixOnMultiply = errors.New("invalid matrix on multiply")
 )
 
 type ADT interface {
 	Transpose() *Matrix
+	QuickTranspose() *Matrix
+	Normalize() [][]interface{}
+	NormalizePrint()
 	Print()
 }
 
@@ -19,7 +27,7 @@ type Matrix struct {
 type Point struct {
 	I    int // 行标
 	J    int // 列标
-	Data interface{}
+	Data int
 }
 
 func Init(row int, col int, points []Point) *Matrix {
@@ -67,34 +75,35 @@ func (m *Matrix) QuickTranspose() *Matrix {
 	// 每一列有几个
 	nums := make([]int, m.Col)
 	// 每一列的第一个 在转换之后应该在的位置
-	cpot := make([]int, m.Col)
+	// 每一列第一个非零元所在的位置
+	colPos := make([]int, m.Col)
 	for _, point := range m.Points {
 		nums[point.J-1]++
 	}
-	cpot[0] = 1
-	for i := 1; i < len(cpot); i++ {
-		cpot[i] = cpot[i-1] + nums[i-1]
+	colPos[0] = 1
+	for i := 1; i < len(colPos); i++ {
+		colPos[i] = colPos[i-1] + nums[i-1]
 	}
 
 	for _, point := range m.Points {
-		r.Points[cpot[point.J-1]-1] = Point{
+		r.Points[colPos[point.J-1]-1] = Point{
 			I:    point.J,
 			J:    point.I,
 			Data: point.Data,
 		}
-		cpot[point.J-1]++
+		colPos[point.J-1]++
 	}
 
 	return r
 }
 
 func (m *Matrix) Print() {
-	fmt.Println(m.Points)
+	//fmt.Println(m.Points)
 
 	count := 0
 	for i := 1; i <= m.Row; i++ {
 		for j := 1; j <= m.Col; j++ {
-			if m.Points[count].I == i && m.Points[count].J == j {
+			if count < len(m.Points) && m.Points[count].I == i && m.Points[count].J == j {
 				fmt.Print(m.Points[count].Data, " ")
 				count++
 			} else {
@@ -103,4 +112,72 @@ func (m *Matrix) Print() {
 		}
 		fmt.Println()
 	}
+	fmt.Println()
+}
+
+func (m *Matrix) Normalize() [][]interface{} {
+	res := make([][]interface{}, m.Row)
+	for i := 0; i < len(res); i++ {
+		res[i] = make([]interface{}, m.Col)
+	}
+
+	for _, v := range m.Points {
+		res[v.I-1][v.J-1] = v.Data
+	}
+
+	return res
+}
+
+func (m *Matrix) NormalizePrint() {
+	matrix := m.Normalize()
+	for _, raw := range matrix {
+		for _, point := range raw {
+			if point != nil {
+				fmt.Print(point, " ")
+				continue
+			}
+			fmt.Print("0", " ")
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func (M *Matrix) Multiply(N *Matrix) (res *Matrix, err error) {
+	if M.Col != N.Row {
+		return nil, ErrInvalidMatrixOnMultiply
+	}
+
+	res = &Matrix{
+		Points: make([]Point, 0, 100),
+		Row:    M.Row,
+		Col:    N.Col,
+		Count:  0,
+	}
+
+	for i := 0; i < M.Row; i++ {
+		// count 可能有坑
+		for j := 0; j < N.Col; j++ {
+			var temp int
+			for m := 0; m < M.Count; m++ {
+				for n := 0; n < N.Count; n++ {
+					if M.Points[m].I-1 == i && N.Points[n].J-1 == j && M.Points[m].J == N.Points[n].I {
+						fmt.Println(M.Points[m], " ", N.Points[n])
+						temp = temp + M.Points[m].Data*N.Points[n].Data
+						//fmt.Println(temp)
+					}
+					//fmt.Println(1)
+				}
+			}
+			if temp != 0 {
+				res.Points = append(res.Points, Point{
+					I:    i+1,
+					J:    j+1,
+					Data: temp,
+				})
+			}
+		}
+	}
+	fmt.Println(res.Points)
+	return res, nil
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -3,19 +3,52 @@ package matrix
 import "testing"
 
 func TestMatrix(T *testing.T) {
-	testInput := [][3]int{{1, 1, 3}, {1, 2, 5}, {2, 3, 7}, {2, 5, 11}, {3, 3, 2}, {3, 6, 8}, {5, 5, 9}, {5, 6, 10}}
-	points := make([]Point, 0, 100)
+	//testInput := [][3]int{{1, 1, 3}, {1, 2, 5}, {2, 3, 7}, {2, 5, 11}, {3, 3, 2}, {3, 6, 8}, {5, 5, 9}, {5, 6, 10}}
+	//points := make([]Point, 0, 100)
+	//
+	//for _, ints := range testInput {
+	//	points = append(points, Point{
+	//		I:    ints[0],
+	//		J:    ints[1],
+	//		Data: ints[2],
+	//	})
+	//}
+	//
+	//m := Init(5, 6, points)
+	//m.Print()
+	//m.NormalizePrint()
+	//t := m.QuickTranspose()
+	//t.NormalizePrint()
 
-	for _, ints := range testInput {
-		points = append(points, Point{
+	testInput1 := [][3]int{{1, 1, 3}, {1, 4, 5}, {2, 2, -1}, {3, 1, 2}}
+	points1 := make([]Point, 0, 100)
+
+	for _, ints := range testInput1 {
+		points1 = append(points1, Point{
 			I:    ints[0],
 			J:    ints[1],
 			Data: ints[2],
 		})
 	}
-
-	m := Init(5, 6, points)
+	m := Init(3, 4, points1)
 	m.Print()
-	t := m.QuickTranspose()
-	t.Print()
+
+	testInput2 := [][3]int{{1, 2, 2}, {2, 1, 1}, {3, 1, -2}, {3, 2, -4}}
+	points2 := make([]Point, 0, 100)
+
+	for _, ints := range testInput2 {
+		points2 = append(points2, Point{
+			I:    ints[0],
+			J:    ints[1],
+			Data: ints[2],
+		})
+	}
+	n := Init(4, 2, points2)
+	n.Print()
+
+	r, err := m.Multiply(n)
+	if err != nil {
+		T.Error(err)
+	}
+	r.Print()
 }
