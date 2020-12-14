ebd9cf3ada2ad046429c2c74ac601a2efbd9eea6
hexagram <xml.hexagram@gmail.com>
2020-12-14 15:58:42 +0800

    整理图

--- graph/graph.go
+++ graph/graph.go
@@ -7,26 +7,26 @@ import (
 )
 
 type ADT interface {
-	LocateVertexByData(data interface{}) (index int, err error)
-	AddVertex(data interface{}) (err error)
-	AddArcByIndex(fromIndex int, toIndex int) (err error)
-	AddArcByVertexData(fromData interface{}, toData interface{}) (err error)
-	FirstAdjVertex(index int) (int, error)
-	NextAdjVertex(index int, start int) int
-	Print()
+	LocateVertex(data interface{}) (index int, err error)
+	FirstAdjVertex(index int) (resIndex int, err error)
 }
 
-var (
-	ErrorNotFound     = errors.New("not found")
-	ErrorAlreadyExist = errors.New("already exist")
+const (
+	Infinity = 99999
 )
 
-const (
-	DG  = iota // 有向图 DiGraph
-	UDG        // 无向图 Undigraph
-	DN         // 有向网
-	UDN        // 无向网
+var (
+	ErrorNotFound      = errors.New("not found")
+	ErrInvalidData     = errors.New("invalid data")
+	ErrNoMoreAdjVertex = errors.New("no more adj vertex")
 )
+
+//const (
+//	DG  = iota // 有向图 DiGraph
+//	UDG        // 无向图 Undigraph
+//	DN         // 有向网
+//	UDN        // 无向网
+//)
 
 type Arc struct {
 	Data interface{}
@@ -34,7 +34,7 @@ type Arc struct {
 }
 
 type Vertex struct {
-	data interface{}
+	Data interface{}
 }
 
 // Matrix Graph
@@ -60,17 +60,54 @@ func New(maxVertexNum int) *MGraph {
 	}
 }
 
-func (g *MGraph) LocateVertexByData(data interface{}) (index int, err error) {
+func CreateGraph(vertexes []interface{}, arcMatrix [][]interface{}) (g *MGraph, err error) {
+	if len(vertexes) != len(arcMatrix) {
+		return nil, ErrInvalidData
+	}
+
+	g = New(len(vertexes))
+	for _, vertex := range vertexes {
+		err = g.AddVertex(vertex)
+		if err != nil {
+			return nil, err
+		}
+	}
+	for i, arcs := range arcMatrix {
+		for j, arc := range arcs {
+			err = g.AddArcByIndex(i, j, arc)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return g, nil
+}
+
+func (g *MGraph) LocateVertex(data interface{}) (index int, err error) {
 	for i := 0; i < g.VertexNumber; i++ {
-		if g.Vertexes[i].data == data {
+		if g.Vertexes[i].Data == data {
 			return i, nil
 		}
 	}
 	return -1, ErrorNotFound
 }
 
+func (g *MGraph) FirstAdjVertex(index int) (resIndex int, err error) {
+	resIndex, err = g.NextAdjVertex(index, 0)
+	return
+}
+
+func (g *MGraph) NextAdjVertex(index int, start int) (resIndex int, err error) {
+	for i := start + 1; i < g.VertexNumber; i++ {
+		if g.ArcMatrix[index][i].Data != 0 {
+			return i, nil
+		}
+	}
+	return -1, ErrNoMoreAdjVertex
+}
+
 func (g *MGraph) AddVertex(data interface{}) (err error) {
-	g.Vertexes[g.VertexNumber].data = data
+	g.Vertexes[g.VertexNumber].Data = data
 	g.VertexNumber++
 	return nil
 }
@@ -93,12 +130,12 @@ func (g *MGraph) AddArcByIndex(fromIndex int, toIndex int, data interface{}) (er
 //	return nil
 //}
 
-func (g *MGraph) AddArcByVertexData(fromData interface{}, toData interface{}, data interface{}) (err error) {
-	fromIndex, err := g.LocateVertexByData(fromData)
+func (g *MGraph) AddArcByData(fromData interface{}, toData interface{}, data interface{}) (err error) {
+	fromIndex, err := g.LocateVertex(fromData)
 	if err != nil {
 		return err
 	}
-	toIndex, err := g.LocateVertexByData(toData)
+	toIndex, err := g.LocateVertex(toData)
 	if err != nil {
 		return err
 	}
@@ -107,24 +144,6 @@ func (g *MGraph) AddArcByVertexData(fromData interface{}, toData interface{}, da
 		return err
 	}
 	return nil
-}
-
-func (g *MGraph) FirstAdjVertex(index int) (resIndex int, hasAdj bool) {
-	for i := 0; i < g.VertexNumber; i++ {
-		if g.ArcMatrix[index][i].Data != 0 {
-			return i, true
-		}
-	}
-	return -1, false
-}
-
-func (g *MGraph) NextAdjVertex(index int, start int) (resIndex int, hasNext bool) {
-	for i := start + 1; i < g.VertexNumber; i++ {
-		if g.ArcMatrix[index][i].Data != 0 {
-			return i, true
-		}
-	}
-	return -1, false
 }
 
 func (g *MGraph) Print() {
@@ -138,34 +157,37 @@ func (g *MGraph) Print() {
 }
 
 // 深度优先搜索
-func (g *MGraph) DFSTraverse(visit func(vertex Vertex)) {
+func (g *MGraph) DFSTraverse(visit func(vertex Vertex) interface{}) (res []interface{}) {
+	res = make([]interface{}, 0, 100)
 	var visited []bool = make([]bool, g.VertexNumber)
 	for i := 0; i < g.VertexNumber; i++ {
 		if !visited[i] {
-
-			g.DFS(i, visit, &visited)
+			g.DFS(i, visit, &visited, &res)
 		}
 	}
-
+	return res
 }
 
-func (g *MGraph) DFS(index int, visit func(vertex Vertex), visited *[]bool) {
+func (g *MGraph) DFS(index int, visit func(vertex Vertex) interface{}, visited *[]bool, res *[]interface{}) {
 	(*visited)[index] = true
-	visit(g.Vertexes[index])
+	*res = append(*res, visit(g.Vertexes[index]))
+
 	for w, _ := g.FirstAdjVertex(index); w >= 0; w, _ = g.NextAdjVertex(index, w) {
 		if !(*visited)[w] {
-			g.DFS(w, visit, visited)
+			g.DFS(w, visit, visited, res)
 		}
 	}
 }
 
-func (g *MGraph) BFSTraverse(visit func(vertex Vertex)) {
+// 广度优先搜索
+func (g *MGraph) BFSTraverse(visit func(vertex Vertex) interface{}) (res []interface{}) {
+	res = make([]interface{}, 0, 100)
 	var visited []bool = make([]bool, g.VertexNumber)
 	q := queue.Init(301)
 	for i := 0; i < g.VertexNumber; i++ {
 		if !visited[i] {
 			visited[i] = true
-			visit(g.Vertexes[i])
+			res = append(res, visit(g.Vertexes[i]))
 			q.Put(i)
 			for !q.IsEmpty() {
 				interfaceData, _ := q.Poll()
@@ -173,33 +195,37 @@ func (g *MGraph) BFSTraverse(visit func(vertex Vertex)) {
 				for w, _ := g.FirstAdjVertex(index); w >= 0; w, _ = g.NextAdjVertex(index, w) {
 					if !visited[w] {
 						visited[w] = true
-						visit(g.Vertexes[w])
+						res = append(res, visit(g.Vertexes[w]))
 						q.Put(w)
 					}
 				}
 			}
 		}
 	}
+	return res
 }
 
 // Dijkstra算法
-func (g *MGraph) Dijkstra() {
+func (g *MGraph) Dijkstra() []int {
 	var dis []int = make([]int, g.VertexNumber)
 	var res []bool = make([]bool, g.VertexNumber)
-	var min = 999
+	var min = Infinity
 	var index int
 
 	for i := 0; i < g.VertexNumber; i++ {
-		if g.ArcMatrix[0][i].Data == nil {
-			dis[i] = 999
+		//hack
+		if g.ArcMatrix[0][i].Data == nil || g.ArcMatrix[0][i].Data.(int) == 0 {
+			dis[i] = Infinity
 			continue
 		}
 		dis[i] = g.ArcMatrix[0][i].Data.(int)
 	}
+	//hack
+	dis[0] = 0
 	res[0] = true
 
 	for i := 1; i < g.VertexNumber; i++ {
-		min = 999
+		min = Infinity
 		for j := 0; j < g.VertexNumber; j++ {
 			if res[j] == false && dis[j] < min {
 				min = dis[j]
@@ -211,12 +237,11 @@ func (g *MGraph) Dijkstra() {
 			if g.ArcMatrix[index][k].Data != 0 && g.ArcMatrix[index][k].Data != nil {
 				if dis[k] > dis[index]+g.ArcMatrix[index][k].Data.(int) {
 					dis[k] = dis[index] + g.ArcMatrix[index][k].Data.(int)
+					fmt.Println(dis)
 				}
 			}
 		}
 	}
 
-	for i, re := range dis {
-		fmt.Println(i, ":", re)
-	}
+	return dis
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -1,34 +1,35 @@
 package graph
 
 import (
+	"fmt"
 	"testing"
 )
 
+func visit(vertex Vertex) interface{} {
+	return vertex.Data
+}
+
 func TestMGraph(t *testing.T) {
-	g := New(6)
-	g.AddVertex("1")
-	g.AddVertex("2")
-	g.AddVertex("3")
-	g.AddVertex("4")
-	g.AddVertex("5")
-	g.AddVertex("6")
+	var vertexes1 []interface{} = []interface{}{"v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"}
+	var arcMatrix1 [][]interface{} = [][]interface{}{
+		{0, 1, 1, 0, 0, 0, 0, 0},
+		{1, 0, 0, 1, 1, 0, 0, 0},
+		{1, 0, 0, 0, 0, 1, 1, 0},
+		{0, 1, 0, 0, 0, 0, 0, 1},
+		{0, 1, 0, 0, 0, 0, 0, 1},
+		{0, 0, 1, 0, 0, 0, 1, 0},
+		{0, 0, 1, 0, 0, 1, 0, 0},
+		{0, 0, 0, 1, 1, 0, 0, 0}}
 
-	g.AddArcByVertexData("1","2",1)
-	g.AddArcByVertexData("1","3",12)
-	g.AddArcByVertexData("2","3",9)
-	g.AddArcByVertexData("2","4",3)
-	g.AddArcByVertexData("4","3",4)
-	g.AddArcByVertexData("3","5",5)
-	g.AddArcByVertexData("4","5",13)
-	g.AddArcByVertexData("4","6",15)
-	g.AddArcByVertexData("5","6",4)
-	g.AddArcByVertexData("1","1",0)
-	g.AddArcByVertexData("2","2",0)
-	g.AddArcByVertexData("3","3",0)
-	g.AddArcByVertexData("4","4",0)
-	g.AddArcByVertexData("5","5",0)
-	g.AddArcByVertexData("6","6",0)
-
+	g, err := CreateGraph(vertexes1, arcMatrix1)
+	if err != nil {
+		t.Error(err)
+	}
 	g.Print()
-	g.Dijkstra()
+	res := g.BFSTraverse(visit)
+	fmt.Println(res)
+	res = g.DFSTraverse(visit)
+
+	fmt.Println(res)
+	fmt.Println(g.Dijkstra())
 }
